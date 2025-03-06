22be05b21c1b6505d3f6d5ff6275979b6b839dc5
Goostaf <gasplund2@gmail.com>
2025-03-06 15:42:12 +0100

    Add basic API service to GoCardless

---
+++ src/services/goCardlessService.ts
@@ -0,0 +1,190 @@
+import { revalidateTag, unstable_cache } from 'next/cache';
+
+const secretId = process.env.GOCARDLESS_SECRET_ID;
+const secretKey = process.env.GOCARDLESS_SECRET_KEY;
+
+interface RequisitionRequest {
+  redirect: string;
+  institution_id: string;
+  reference: string;
+  agreement?: string;
+  user_language?: string;
+  ssn?: string;
+  account_selection?: boolean;
+  redirect_immediate?: boolean;
+}
+
+interface AccessToken {
+  access: string;
+  access_expires: number;
+  refresh: string;
+  refresh_expires: number;
+}
+
+export default class GoCardlessService {
+  static apiUrl = 'https://bankaccountdata.gocardless.com/api/v2';
+
+  private static async newToken() {
+    const response = await fetch(this.apiUrl + '/token/new/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        secret_id: secretId,
+        secret_key: secretKey
+      })
+    });
+
+    if (!response.ok) {
+      const errorData = await response
+        .json()
+        .then((j) => JSON.stringify(j))
+        .catch(() => response.statusText);
+      throw new Error(
+        `GoCardless request failed with status ${response.status}`,
+        {
+          cause: errorData
+        }
+      );
+    }
+
+    return (await response.json()) as AccessToken;
+  }
+
+  private static async refreshToken(refreshToken: string) {
+    const response = await fetch(this.apiUrl + '/token/refresh/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        refresh: refreshToken
+      })
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => response.statusText);
+      throw new Error(
+        `GoCardless request failed with status ${response.status}`,
+        {
+          cause: errorData
+        }
+      );
+    }
+
+    return (await response.json()) as {
+      access: string;
+      access_expires: number;
+    };
+  }
+
+  private static async getToken() {
+    const tokenCache = unstable_cache(
+      async (oldToken?: AccessToken) => {
+        console.log('Refreshing token');
+        const requestAt = Date.now() / 1000;
+
+        return {
+          res: oldToken
+            ? {
+                ...(await this.refreshToken(oldToken.refresh)),
+                refresh: oldToken.refresh,
+                refresh_expires: oldToken.refresh_expires
+              }
+            : await this.newToken(),
+          time: requestAt
+        };
+      },
+      ['gocardless-token'],
+      { tags: ['gocardless-token'], revalidate: false }
+    );
+
+    const cachedToken = await tokenCache();
+
+    if (
+      cachedToken.time + cachedToken.res.refresh_expires - 60 <
+      Date.now() / 1000
+    ) {
+      console.log('Refresh token expired');
+      revalidateTag('gocardless-token');
+      return (await tokenCache()).res;
+    } else if (
+      cachedToken.time + cachedToken.res.access_expires - 60 <
+      Date.now() / 1000
+    ) {
+      console.log('Access token expired');
+      revalidateTag('gocardless-token');
+      return (await tokenCache(cachedToken.res)).res;
+    }
+
+    return cachedToken.res;
+  }
+
+  static async getBankAccountBalance(id: string) {
+    const response = await fetch(
+      this.apiUrl + '/accounts/' + id + '/balances/',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + (await this.getToken()).access,
+          accept: 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response
+        .json()
+        .then((j) => JSON.stringify(j))
+        .catch(() => response.statusText);
+      throw new Error(
+        `GoCardless request failed with status ${response.status}`,
+        {
+          cause: errorData
+        }
+      );
+    }
+
+    return (await response.json()) as {
+      balances: [
+        {
+          balanceAmount: {
+            amount: number;
+            currency: string;
+          };
+          balanceType: 'interimAvailable' | 'interimBooked';
+        }
+      ];
+    };
+  }
+
+  static async createRequisition(r: RequisitionRequest) {
+    const response = await fetch(this.apiUrl + '/requisitions/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + (await this.getToken()).access
+      },
+      body: JSON.stringify(r)
+    });
+
+    if (!response.ok) {
+      const errorData = await response
+        .json()
+        .then((j) => JSON.stringify(j))
+        .catch(() => response.statusText);
+      throw new Error(
+        `GoCardless request failed with status ${response.status}`,
+        {
+          cause: errorData
+        }
+      );
+    }
+
+    return (await response.json()) as {
+      balance: number;
+      currency: string;
+    };
+  }
+}
